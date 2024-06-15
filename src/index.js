495b7be7e3305632da3fd02e7e1ecf4949869d64
huynamboz <teahuynam2003@gmail.com>
2024-06-16 04:56:45 +0700

    update

--- src/index.js
+++ src/index.js
@@ -15,16 +15,63 @@ mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   const wss = new WebSocket.Server({ host: '0.0.0.0', port: 5001 });
   logger.info('WebSocket server is listening on port 5001');
 
+  // Store clients with their associated rasID
+  const clients = new Map();
+
   wss.on('connection', (ws) => {
     logger.info('New WebSocket connection');
 
     ws.on('message', (message) => {
-      logger.info(`Received message: ${message}`);
-      // Handle incoming messages here
+      try {
+        const parsedMessage = JSON.parse(message);
+        logger.info(`Received message: ${JSON.stringify(parsedMessage)}`);
+
+        if (parsedMessage.rasID) {
+          // Store the rasID with the client
+          // clients.set(ws, parsedMessage.rasID);
+          // logger.info(`Stored rasID ${parsedMessage.rasID} for client`);
+          if (!clients.has(ws)) {
+            clients.set(ws, parsedMessage.rasID);
+            logger.info(`Stored rasID ${parsedMessage.rasID} for client`);
+          }
+        }
+
+        if (parsedMessage.ID) {
+          // check if not have ID before set
+          if (!clients.has(ws)) {
+            clients.set(ws, parsedMessage.ID);
+          }
+          // Forward the message to the client with the matching rasID
+        }
+        // clients.forEach((rasID, client) => {
+        //   if (rasID === parsedMessage.ID && client.readyState === WebSocket.OPEN && client !== ws) {
+        //     client.send(JSON.stringify(parsedMessage));
+        //     logger.info(`Forwarded message to client with rasID ${rasID}`);
+        //   }
+        // });
+        // send for client have same ID == rasID
+        clients.forEach((rasID, client) => {
+          if (rasID === parsedMessage.ID && client.readyState === WebSocket.OPEN && client !== ws) {
+            client.send(JSON.stringify(parsedMessage));
+            logger.info(`Forwarded message to client with rasID ${rasID}`);
+          }
+        });
+
+        clients.forEach((ID, client) => {
+          if (ID === parsedMessage.rasID && client.readyState === WebSocket.OPEN && client !== ws) {
+            client.send(JSON.stringify(parsedMessage));
+            logger.info(`Forwarded message to client with ID ${ID}`);
+          }
+        });
+
+      } catch (e) {
+        logger.error('Failed to parse message', e);
+      }
     });
 
     ws.on('close', () => {
       logger.info('WebSocket connection closed');
+      clients.delete(ws);
     });
   });
 });
